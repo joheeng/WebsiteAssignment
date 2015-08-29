8f670e3fc9d2ef23e07cc5db1a6779a77044e9f3
mwei0210 <mwei0210@gmail.com>
2015-08-30 07:22:26 +0800

    "cart button" at top left updates upon "add to cart" click
    checkStorage() added and run on page load

--- js/main.js
+++ js/main.js
@@ -13,15 +13,15 @@ jQuery(document).ready(function($){
         responsive:{
             0:{
                 items:1,
-            },
+			},
             600:{
                 items:3,
-            },
+			},
             1000:{
                 items:5,
-            }
-        }
-    });  
+			}
+		}
+	});  
     
     $('.related-products-carousel').owlCarousel({
         loop:true,
@@ -31,18 +31,18 @@ jQuery(document).ready(function($){
         responsive:{
             0:{
                 items:1,
-            },
+			},
             600:{
                 items:2,
-            },
+			},
             1000:{
                 items:2,
-            },
+			},
             1200:{
                 items:3,
-            }
-        }
-    });  
+			}
+		}
+	});  
     
     $('.brand-list').owlCarousel({
         loop:true,
@@ -52,21 +52,21 @@ jQuery(document).ready(function($){
         responsive:{
             0:{
                 items:1,
-            },
+			},
             600:{
                 items:3,
-            },
+			},
             1000:{
                 items:4,
-            }
-        }
-    });    
+			}
+		}
+	});    
     
     
     // Bootstrap Mobile Menu fix
     $(".navbar-nav li a").click(function(){
         $(".navbar-collapse").removeClass('in');
-    });    
+	});    
     
     // jQuery Scroll effect
     $('.navbar-nav li a, .scroll-to-up').bind('click', function(event) {
@@ -74,22 +74,47 @@ jQuery(document).ready(function($){
         var headerH = $('.header-area').outerHeight();
         $('html, body').stop().animate({
             scrollTop : $($anchor.attr('href')).offset().top - headerH + "px"
-        }, 1200, 'easeInOutExpo');
-
+		}, 1200, 'easeInOutExpo');
+		
         event.preventDefault();
-    });    
+	});    
     
     // Bootstrap ScrollPSY
     $('body').scrollspy({ 
         target: '.navbar-collapse',
         offset: 95
-    })      
+	})      
 });
 
 
 $('.view-details-link').click(function(){
 	var pid = $(this).parents().eq(2).attr("id");
-   window.location.href = "product?pid="+ pid;
+	window.location.href = "product?pid="+ pid;
+	
 });
 
+$('.add-to-cart-link').click(function(){
+	var pid = $(this).parents().eq(2).attr("id");
+	var cartItem = JSON.parse(localStorage["cartItem"]);
+	cartItem.push(pid);
+	localStorage["cartItem"] = JSON.stringify(cartItem);
+	checkStorage();
+});
+
+
+function checkStorage() {
+	if(localStorage.length == 0) {
+		
+		var cartItem = [];
+		localStorage["cartItem"] = JSON.stringify(cartItem);
+		
+	} else {
+		$(".product-count").each(function() {
+			
+			$(this).html(JSON.parse(localStorage["cartItem"]).length);
+			
+		});		
+		
+	}
+}
 
